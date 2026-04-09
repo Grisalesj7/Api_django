e0148de93567f77142ec418c38370a6e4c1c460e
Johao <grisalesjohao@gmail.com>
2026-04-09 16:53:21 -0500

    Corregir ruta en build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
-# Salir inmediatamente si ocurre un error
+# Salir si hay un error
 set -o errexit
 
-# 1. Instalar las librerías del requirements.txt
+# Instalar dependencias (esto se hace en la raíz)
 pip install -r requirements.txt
 
-# 2. Recolectar archivos estáticos (CSS, Imágenes, JS)
-# Esto es vital para que tu página no se vea sin diseño en Render
+# --- EL CAMBIO ESTÁ AQUÍ ---
+# Entrar a la carpeta donde está manage.py
+cd sistema_pedidos
+
+# Ahora sí, los comandos de Django
+python manage.py migrate
 python manage.py collectstatic --no-input
 
-# 3. Aplicar las tablas a la base de datos de la nube
-python manage.py migrate
-
-# Crear superusuario automáticamente si no existe
+# Crear superusuario
 export DJANGO_SUPERUSER_PASSWORD="1234"
 python manage.py createsuperuser --noinput --username admin --email admin@ejemplo.com || true
