daacf613cdc8c4ff24f8364bf45be1084de54de0
Johao <grisalesjohao@gmail.com>
2026-04-09 17:06:51 -0500

    Solucion definitiva: Migraciones con ruta corregida

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+# Salir si hay un error
 set -o errexit
 
-echo "--- LISTADO DE ARCHIVOS EN LA RAÍZ ---"
-ls -R
-
-echo "--- INTENTANDO INSTALAR ---"
+# Instalar dependencias
 pip install -r requirements.txt
 
-echo "--- BUSCANDO MANAGE.PY ---"
-find . -name "manage.py"
+# Entrar a la carpeta donde está el manage.py
+cd sistema_pedidos
+
+# Ejecutar las migraciones para crear las tablas
+python manage.py migrate
+
+# Recolectar archivos estáticos (CSS, Imágenes)
+python manage.py collectstatic --no-input
+
+# Crear tu usuario (Cámbialos por los tuyos)
+export DJANGO_SUPERUSER_PASSWORD="1234"
+python manage.py createsuperuser --noinput --username admin --email admin@ejemplo.com || true
