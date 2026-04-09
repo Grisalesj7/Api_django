b663e0f42c2b2ed84dec7fb99e39ac0cfcffc1bd
Johao <grisalesjohao@gmail.com>
2026-04-09 16:58:58 -0500

    Diagnóstico de carpetas

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
-# Salir si hay un error
 set -o errexit
 
-# Instalar dependencias
+echo "--- LISTADO DE ARCHIVOS EN LA RAÍZ ---"
+ls -R
+
+echo "--- INTENTANDO INSTALAR ---"
 pip install -r requirements.txt
 
-# Ejecutar comandos usando la ruta del archivo manage.py desde la raíz
-# Si tu archivo está en sistema_pedidos/manage.py, usamos esa ruta:
-python sistema_pedidos/manage.py migrate
-python sistema_pedidos/manage.py collectstatic --no-input
-
-# Crear superusuario
-export DJANGO_SUPERUSER_PASSWORD="1234"
-python sistema_pedidos/manage.py createsuperuser --noinput --username admin --email admin@ejemplo.com || true
+echo "--- BUSCANDO MANAGE.PY ---"
+find . -name "manage.py"
