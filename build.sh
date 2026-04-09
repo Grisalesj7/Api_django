d813c4852df7117c3a9dedbbcb58cc999e7aa2ee
Johao <grisalesjohao@gmail.com>
2026-04-09 15:55:30 -0500

    Agregar build.sh con permisos de ejecucion

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Salir inmediatamente si ocurre un error
+set -o errexit
+
+# 1. Instalar las librerías del requirements.txt
+pip install -r requirements.txt
+
+# 2. Recolectar archivos estáticos (CSS, Imágenes, JS)
+# Esto es vital para que tu página no se vea sin diseño en Render
+python manage.py collectstatic --no-input
+
+# 3. Aplicar las tablas a la base de datos de la nube
+python manage.py migrate
