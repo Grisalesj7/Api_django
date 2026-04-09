df9dbe2a6cec59de6bc09eb7439ece89146e4e1c
Johao <grisalesjohao@gmail.com>
2026-04-09 16:44:10 -0500

    Crear superusuario desde build script

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ python manage.py collectstatic --no-input
 
 # 3. Aplicar las tablas a la base de datos de la nube
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+export DJANGO_SUPERUSER_PASSWORD="1234"
+python manage.py createsuperuser --noinput --username admin --email admin@ejemplo.com || true
